e552b0db35a055e05cb6cb2c76497cb57428fd41
Mzahlut <mzahlut@gmail.com>
2026-05-31 20:13:12 -0300

    Initial configs

---
+++ Client/Nido/src/tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+
+/** @type { import('tailwindcss').Config } */
+
+import daisyui from 'daisyui'
+
+export default {
+    content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [daisyui],
+    daisyui: {
+        themes: ['dark'],
+    },
+}
